40440e330f8a7f6e0380bb7265c83e234a82fcc7
dldemoks <demokstar@gmail.com>
2017-02-25 15:02:57 +0700

    update

--- admin/controller/payment/payeer.php
+++ admin/controller/payment/payeer.php
@@ -164,15 +164,6 @@ class ControllerPaymentpayeer extends Controller {
 			$data['payeer_security'] = $this->config->get('payeer_security');
 		}
 
-		if (isset($this->request->post['payeer_sort_order'])) 
-		{
-			$data['payeer_sort_order'] = $this->request->post['payeer_sort_order'];
-		} 
-		else 
-		{
-			$data['payeer_sort_order'] = $this->config->get('payeer_sort_order');
-		}
-
 		if (isset($this->request->post['payeer_order_wait_id'])) 
 		{
 			$data['payeer_order_wait_id'] = $this->request->post['payeer_order_wait_id'];
